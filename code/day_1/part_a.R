095782820fe9884025fe3ac3d6fa18e12e29d003
David Arbelaez <arbdavid2@gmail.com>
2022-06-14 23:56:02 -0400

    part a intro

---
+++ code/day_1/part_a.R
@@ -0,0 +1,10 @@
+################# RSTUDIO INTRO ################################################
+# Layouts
+# Backgrounds
+
+a_tibble <- dplyr::tibble(x = runif(10), y = runif(10))
+
+View(a_tibble)
+View(a_tibble, 'Test')
+
+#
